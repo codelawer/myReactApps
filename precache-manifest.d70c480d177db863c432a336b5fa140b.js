84501c995b5af5e53f1eb4228c8dd3922d441340
codelawer <codelawer@gmail.com>
2020-01-10 23:53:08 +0100

    Updates

---
+++ precache-manifest.d70c480d177db863c432a336b5fa140b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50b686239d3cc650ce601be9149f127",
+    "url": "/myReactApps/index.html"
+  },
+  {
+    "revision": "43be6c111940068cae68",
+    "url": "/myReactApps/static/css/main.6fecc0e7.chunk.css"
+  },
+  {
+    "revision": "a4706ab797584e3ce1cc",
+    "url": "/myReactApps/static/js/2.749fe5f5.chunk.js"
+  },
+  {
+    "revision": "43be6c111940068cae68",
+    "url": "/myReactApps/static/js/main.6e149bcf.chunk.js"
+  },
+  {
+    "revision": "17f15d5c1aa1b0a835a8",
+    "url": "/myReactApps/static/js/runtime~main.5f1f1ba3.js"
+  }
+]);
